80521736f9503acde06fe0bc5dd606238d55b38e
IKPreethi <preethiik06@gmail.com>
2023-10-15 22:48:19 -0500

    LandingPage

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+         <Route index element={<Home />} />
+          {/* <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
+import './Home.scss'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = ['P', 'r', 'e', 'e', 't', 'h', 'i']
+  const jobArray = [
+    'S',
+    'o',
+    'f',
+    't',
+    'w',
+    'a',
+    'r',
+    'e',
+    ' ',
+    'D',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+     setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  const title = (
+    <div className="container home-page">
+    <div className="text-zone">
+      <h1>
+        <span className={letterClass}>H</span>
+        <span className={`${letterClass} _12`}>i,</span>
+        <br />
+        <span className={`${letterClass} _13`}>I</span>
+        <span className={`${letterClass} _14`}>'m</span>
+        <span> </span>
+        <AnimatedLetters
+          letterClass={letterClass}
+          strArray={nameArray}
+          idx={15}
+        />
+        <br />
+        <AnimatedLetters
+          letterClass={letterClass}
+          strArray={jobArray}
+          idx={22}
+        />
+      </h1>
+      <h2>Full Stack Developer / React Js / .Net / C#</h2>
+    </div>
+  </div>
+  );
+
+  return (
+    <div>
+     {title}
+    </div>
+  )
+}
+
+export default Home
